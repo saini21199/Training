11fa7652c024c8d38cbf3b63256648f59c82d839
saini21199 <sachinsainiandromeda007@gmail.com>
2021-03-31 12:23:56 +0530

    data binding

---
+++ Angular/imageassignment/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ImageComponent } from './image/image.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImageComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
