ad80c2afac80fd474eb210a68441a8246f958329
saini21199 <sachinsainiandromeda007@gmail.com>
2021-04-01 18:17:24 +0530

    Component communication

--- Angular/imageassignment/src/app/app.component.ts
+++ Angular/imageassignment/src/app/app.component.ts
@@ -9,20 +9,30 @@ export class AppComponent {
   title = "Data Binding";
   showDataBindingDemo = true;
   showDirectiveDemo = false;
-
+  showPurchaseDemo=false;
 
   showdbdemo(): void {
     this.showDataBindingDemo = true;
     this.showDirectiveDemo = false;
     this.title="Data Binding"
+    this.showPurchaseDemo=false;
   }
 
   showdirectivedemo(): void {
     this.showDirectiveDemo = true;
     this.showDataBindingDemo = false;
-    
+    this.showPurchaseDemo=false;
     this.title="Displaying products using directive"
 
   }
+
+  showpurchasedemo() : void{
+    this.showDirectiveDemo = false;
+    this.showDataBindingDemo = false;
+    this.showPurchaseDemo=true;
+    this.title="Displaying Purchase component"
+  }
+ 
+ 
 
 }
--- Angular/imageassignment/src/app/app.module.ts
+++ Angular/imageassignment/src/app/app.module.ts
@@ -5,16 +5,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ImageComponent } from './image/image.component';
 import { StructuredirectiveComponent } from './structuredirective/structuredirective.component';
+import { PurchaseComponent } from './purchase/purchase.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PurchaseOrderComponent } from './purchase-order/purchase-order.component';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
     AppComponent,
     ImageComponent,
-    StructuredirectiveComponent
+    StructuredirectiveComponent,
+    PurchaseComponent,
+    PurchaseOrderComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
